7f6683da20b0a12f4f692068d8773b5294c72b11
hendouelboukhary2203-wq <hendouelboukhary2203@gmail.com>
2025-09-19 11:53:17 +0000

    commit

--- app/search/page.js
+++ app/search/page.js
@@ -1,16 +1,15 @@
 // app/search/page.js
-'use client';
-
 export const metadata = { title: "البحث" };
-
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
+import { Suspense } from "react";
+import SearchClient from "./SearchClient";
+
 export default function Page() {
   return (
-    <>search</>
-    // <Suspense fallback={<div style={{ padding: "2rem" }}>جار التحميل...</div>}>
-    //   <SearchPage />
-    // </Suspense>
+    <Suspense fallback={<div style={{ padding: "2rem" }}>جار التحميل...</div>}>
+      <SearchClient />
+    </Suspense>
   );
 }
